dff55a8c2ea7099fb3c5e3ca9adbd064cefc36ac
doanhuynh049 <quocthien049@gmail.com>
2024-01-11 15:29:24 +0700

    update

---
+++ export_toolchain.sh
@@ -0,0 +1,5 @@
+#How to use:
+#.  export_toolchain.sh
+# source export_toolchain.sh
+export PATH=$PATH:/home/quathd/beagleboard_project/toolchain_beaglebone/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf/bin
+arm-linux-gnueabihf-gcc --version
