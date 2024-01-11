ec7dce96712791655db43970ba0e95c7681d612f
doanhuynh049 <quocthien049@gmail.com>
2024-01-11 13:49:12 +0700

    first commit

---
+++ linuxbsp_package_build.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get install build-essential gcc g++ make binutils libncurses-dev zlib1g-dev libssl-dev libelf-dev git bc bison flex pkg-config libglib2.0-dev doxygen graphviz
+
